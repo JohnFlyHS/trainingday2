99c7b9c496659f5166212a656aeee05cf8eae9c9
John Fly <john.fly@healthstream.com>
2017-03-22 11:26:56 -0500

    adding some rspec tests

--- chef-repo/cookbooks/httpd/spec/unit/recipes/default_spec.rb
+++ chef-repo/cookbooks/httpd/spec/unit/recipes/default_spec.rb
@@ -7,14 +7,26 @@
 require 'spec_helper'
 
 describe 'httpd::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on centos 6.7' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '6.7')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+    
+    it 'includes the install recipe' do
+      expect(chef_run).to include_recipe('httpd::install')
+    end
+        
+    it 'includes the configuration recipe' do
+      expect(chef_run).to include_recipe('httpd::configuration')
+    end  
+
+    it 'includes the service recipe' do
+      expect(chef_run).to include_recipe('httpd::service')
+    end  
   end
 end
