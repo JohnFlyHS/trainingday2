6f9523850d4357960edd4853d902ecd6a29bbbae
John Fly <john.fly@healthstream.com>
2017-03-22 14:39:35 -0500

    more changes to custom resource

--- extending_cookbooks/cookbooks/httpd/resources/vhost.rb
+++ extending_cookbooks/cookbooks/httpd/resources/vhost.rb
@@ -1,17 +1,31 @@
+property :site_name, String
+property :site_port, Fixnum
+
 action :create do
-  directory '/srv/apache/admins/html' do
+  directory "/srv/apache/#{site_name}/html" do
     recursive true
     mode '0755'
   end
 
-  template '/etc/httpd/conf.d/admins.conf' do
+  template "/etc/httpd/conf.d/#{site_name}.conf" do
     source 'conf.erb'
     mode '0644'
-    variables(document_root: '/srv/apache/admins/html', port: 8080)
+    variables(document_root: "/srv/apache/#{site_name}/html", port: site_port)
     notifies :restart, 'service[httpd]'
   end
 
-  file '/srv/apache/admins/html/index.html' do
-    content '<h1>Welcome admins!</h1>'
+  file "/srv/apache/#{site_name}/html/index.html" do
+    content "<h1>Welcome #{site_name}!</h1>"
   end
 end
+
+action :remove do
+  directory "/srv/apache/#{site_name}/html" do
+    action :delete
+  end  
+
+  file "/etc/httpd/conf.d/#{site_name}.conf" do
+    action :delete
+  end
+end
+
--- extending_cookbooks/cookbooks/httpd/spec/unit/recipes/default_spec.rb
+++ extending_cookbooks/cookbooks/httpd/spec/unit/recipes/default_spec.rb
@@ -29,12 +29,6 @@ describe 'httpd::default' do
       expect(chef_run).to enable_service('httpd')
     end
 
-    describe 'for the default site' do
-      it 'writes out a new home page' do
-        expect(chef_run).to render_file('/var/www/html/index.html').with_content('<h1>Welcome home!</h1>')
-      end
-    end
-
     describe 'for the admin site' do
       it 'creates the directory' do
         expect(chef_run).to create_directory('/srv/apache/admins/html')
@@ -47,7 +41,21 @@ describe 'httpd::default' do
       it 'creates a new home page' do
         expect(chef_run).to render_file('/srv/apache/admins/html/index.html').with_content('<h1>Welcome admins!</h1>')
       end
-      
     end
+
+    describe 'for the user site' do
+      it 'creates the directory' do
+        expect(chef_run).to create_directory('/srv/apache/users/html')
+      end
+
+      it 'creats the configuration' do
+        expect(chef_run).to render_file('/etc/httpd/conf.d/users.conf')
+      end
+
+      it 'creates a new home page' do
+        expect(chef_run).to render_file('/srv/apache/users/html/index.html').with_content('<h1>Welcome users!</h1>')
+      end      
+    end
+
   end
 end
